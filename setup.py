46609eaaf0dc8cc7aa6c14db5f1c64fdab447b1e
mcbal <matthiascbal@gmail.com>
2021-04-25 11:34:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="deep-implicit-attention",
-    packages=find_packages(exclude=["images"]),
-    version="0.0.0",
-    license="MIT",
-    description="Deep Implicit Attention",
-    author="Matthias Bal",
-    author_email="matthiascbal@gmail.com",
-    url="https://github.com/mcbal/deep-implicit-attention",
-    keywords=["artificial intelligence", "attention mechanism"],
-    install_requires=["numpy>=1.19", "torch>=1.6"],
+    name='deep-implicit-attention',
+    packages=find_packages(exclude=['images']),
+    version='0.0.0',
+    license='MIT',
+    description='Deep Implicit Attention',
+    author='Matthias Bal',
+    author_email='matthiascbal@gmail.com',
+    url='https://github.com/mcbal/deep-implicit-attention',
+    keywords=['artificial intelligence', 'attention mechanism'],
+    install_requires=['einops>=0.3', 'numpy>=1.19', 'torch>=1.8'],
 )
